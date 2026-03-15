47bf7650903f1de47928f35d63faa0c8c0da5e7e
DANR1BEIRO <danribeirodev@gmail.com>
2026-03-14 23:45:26 -0300

    test: implement failing test first
    Based on the TDD principle (writing tests before implementing the application code),
    i started by writing a failing test first. It's important to have a failing test first
    because this approach gives the developer high confidence that whatever they did to fix the test actually worked.

---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,19 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CashCardJsonTest {
+
+    @Test
+    void myFirstTest() {
+        assertThat(1).isEqualTo(42);
+    }
+}
+
+/**
+ * Write a failing test
+ * Use TDD to test a JSON data contract
+ * Use TDD to test JSON deserialization
+ */
