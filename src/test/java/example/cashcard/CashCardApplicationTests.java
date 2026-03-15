f0f60a5d69d129f10b74a998959cf16c5e30245b
DANR1BEIRO <danribeirodev@gmail.com>
2026-03-15 12:40:02 -0300

    test: implement a failing test for GET
    Based on test-first practice, this test describe the expectations `before` the code implementation
    that satisfies those expectations.
    
    - expected: 200 OK
    - but was: 404 NOT_FOUND
    
    Since I haven't insctructed Spring Web how to handle GET cashcards/99, Spring web is automatically
    responding that endpoint is not found.

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,13 +1,28 @@
 package example.cashcard;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import javax.swing.*;
+
+// start our Spring Boot application and make it available for our test to perform requests to it.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    // inject a test helper that'll allow us to make HTTP requests to the locally running application
+    @Autowired
+    TestRestTemplate restTemplate;
 
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        // use `restTemplate` to make an HTTP GET request to our application endpoint `/cashcards/99`
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 }
