6b21d4348bf552332bea004092b5e34af24eed8d
DANR1BEIRO <danribeirodev@gmail.com>
2026-03-19 12:03:49 -0300

    feat: define a Spring Controller to implement the REST read endpoint

---
+++ src/main/java/example/cashcard/controller/CashCardController.java
@@ -0,0 +1,17 @@
+package example.cashcard.controller;
+
+import example.cashcard.CashCard;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CashCardController {
+
+    @GetMapping("/cashcards/{requestedId}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        CashCard cashCard = new CashCard(requestedId, 100.00);
+        return ResponseEntity.ok(cashCard);
+    }
+}
