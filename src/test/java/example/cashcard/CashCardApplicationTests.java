5f3c805b7382ce9c6f4cb016b792d163f240eaea
DANR1BEIRO <danribeirodev@gmail.com>
2026-03-20 11:54:19 -0300

    feat: use TDD to creata the GET endpoint that returns a CashCard of a certain ID

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,6 +1,8 @@
 package example.cashcard;
 
-import org.assertj.core.api.Assertions;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +10,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import javax.swing.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 // start our Spring Boot application and make it available for our test to perform requests to it.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,10 +21,30 @@ class CashCardApplicationTests {
     TestRestTemplate restTemplate;
 
     @Test
+    @DisplayName("Should return a Cash Card when data is saved")
     void shouldReturnACashCardWhenDataIsSaved() {
         // use `restTemplate` to make an HTTP GET request to our application endpoint `/cashcards/99`
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
 
-        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        // CConverts the response string into a json-aware object with lots of helped methods
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(99);
+
+        Double amount = documentContext.read("$.amount");
+        assertThat(amount).isEqualTo(123.45);
+        // expect that when we request a Cash Card with id of 99 a JSON objet will be return with
+        // something in the id field. Assert that the id is not null
+    }
+
+    @Test
+    @DisplayName("Should not return a Cash Card with an unknown id")
+    void shouldNotReturnACashCardWithAnUnknownId() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
     }
 }
