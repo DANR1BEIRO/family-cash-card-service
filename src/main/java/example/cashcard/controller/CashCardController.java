979595d9a54dee768a93db2134c3e4680131e69c
DANR1BEIRO <danribeirodev@gmail.com>
2026-03-22 20:39:47 -0300

    feat: inject CashCardRepository and refactor the findById method to perform based on cashCardRepository method

--- src/main/java/example/cashcard/controller/CashCardController.java
+++ src/main/java/example/cashcard/controller/CashCardController.java
@@ -1,23 +1,29 @@
 package example.cashcard.controller;
 
 import example.cashcard.CashCard;
+import example.cashcard.repository.CashCardRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+    private final CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
 
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        if (requestedId.equals(90)) {
-            CashCard cashCard = new CashCard(90L, 123.45);
-            return ResponseEntity.ok(cashCard);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        return cashCardOptional.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
+
