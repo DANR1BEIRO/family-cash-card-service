04a5d86d6a53a119f3c884a4a91164b11d59e252
DANR1BEIRO <danribeirodev@gmail.com>
2026-03-26 15:28:42 -0300

    feat: extends PagingAndSortingRepository, allowing us to implement a pageable findAll method

--- src/main/java/example/cashcard/repository/CashCardRepository.java
+++ src/main/java/example/cashcard/repository/CashCardRepository.java
@@ -2,7 +2,8 @@ package example.cashcard.repository;
 
 import example.cashcard.CashCard;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
 
