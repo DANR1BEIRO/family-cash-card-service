5339805339c850a3ac597ebc2389256eb9a95e7c
DANR1BEIRO <danribeirodev@gmail.com>
2026-03-29 10:57:11 -0300

    feat: creates a SecurityConfig into a config package that configures and handle basic authentication

---
+++ src/main/java/example/cashcard/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package example.cashcard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * The {@code @Configuration} annotation tells Spring to use this class to configure Spring and Spring Boot itself.
+ * Any Beans specified in this class will now be available to Spring's Auto Configuration engine.
+ */
+@Configuration
+class SecurityConfig {
+
+    /**
+     * Spring Security expects a Bean to configure its Filter Chain.
+     * Annotating a method returning a {@link SecurityFilterChain} with the {@code @Bean} satisfies this expectation.
+     *
+     * @param http O objeto {@link HttpSecurity} usado para montar as regras de segurança.
+     * @return O {@link SecurityFilterChain} que será injetado no contexto do Spring.
+     * @throws Exception Se houver erro nas definições de segurança ou no build do objeto.
+     */
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /**
+         * All HTTP requests to cashcards/ endpoints are required to be authenticated
+         * using HTTP Basic Authentication security (username and password).
+         */
+        http.authorizeHttpRequests(request -> request
+                        .requestMatchers("/cashcards/**")
+                        .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
